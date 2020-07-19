38907de8e427cad3f64601f03b2aac4ae95ab813
Dhananjayan625 <dhananjayan625@gmail.com>
2020-07-19 15:38:37 +0530

    6

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+// const router = express.Router()
 
 app.get('/', (req, res) => {
     res.send('Hello Worlds')
 })
-app.listen(3000., () => {
+app.listen(process.env.PORT || 3000., () => {
     console.log("Server running")
 })
